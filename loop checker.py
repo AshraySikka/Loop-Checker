a66d83716746adc23388749f6d5b09ec9e3fab0f
Ashray Sikka <69213851+AshraySikka@users.noreply.github.com>
2025-05-22 21:52:20 -0400

    Create loop checker.py

---
+++ loop checker.py
@@ -0,0 +1,12 @@
+import math
+for i in range(10):
+    print(i)
+    print("still in loop")
+    print("Still in loop line 2")
+    print("\n")
+print("End loop")
+print ('\n')
+name = "Hello"
+for i in name:
+    print(i)
+ 
